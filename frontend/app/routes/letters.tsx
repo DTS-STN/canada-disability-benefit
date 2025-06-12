f66c1efb1ff296649c3391f55862267a03c8095e
abalogh-esdc <adam.balogh@hrsdc-rhdcc.gc.ca>
2025-06-12 02:07:18 -0400

    Display letters list with test data.

--- frontend/app/routes/letters.tsx
+++ frontend/app/routes/letters.tsx
@@ -1,11 +1,19 @@
+import type { ChangeEvent } from 'react';
+
 import type { RouteHandle } from 'react-router';
+import { useSearchParams } from 'react-router';
 
 import { useTranslation } from 'react-i18next';
+import * as v from 'valibot';
 
-import type { Route } from './+types/index';
+import type { Route } from './+types/letters';
 
 import { requireAuth } from '~/.server/utils/auth-utils';
+import { ButtonLink } from '~/components/button-link';
+import { InputSelect } from '~/components/input-select';
+import { InlineLink } from '~/components/links';
 import { PageTitle } from '~/components/page-title';
+// import { useLanguage } from '~/hooks/use-language';
 import { getTranslation } from '~/i18n-config.server';
 import { handle as parentHandle } from '~/routes/layout';
 
@@ -13,23 +21,107 @@ export const handle = {
   i18nNamespace: [...parentHandle.i18nNamespace],
 } as const satisfies RouteHandle;
 
+const orderEnumSchema = v.picklist(['asc', 'desc']);
+
 export async function loader({ context, params, request }: Route.LoaderArgs) {
   await requireAuth(context.session, request);
   const { t } = await getTranslation(request, handle.i18nNamespace);
-  return { documentTitle: t('app:index.page-title') };
+  // FIXME ::: sort order
+  const sortParam = new URL(request.url).searchParams.get('sort');
+  const fallbackOrderEnumSchema = v.fallback(orderEnumSchema, 'desc');
+  const sortOrder = v.parse(fallbackOrderEnumSchema, sortParam);
+
+  // TODO ::: fetch actual data here
+  const MSCA_BASE_URL = 'http://localhost:3000';
+  // const letters = '';
+  const letters = [
+    { id: '1', date: '2024-12-25', letterTypeId: 'ACC' },
+    { id: '2', date: '2004-02-29', letterTypeId: 'DEN' },
+    { id: '3', date: '2004-02-29', letterTypeId: 'DEN' },
+  ];
+  const letterTypes = [
+    { id: 'ACC', nameEn: 'Accepted', nameFr: '(FR) Accepted' },
+    { id: 'DEN', nameEn: 'Denied', nameFr: '(FR) Denied' },
+  ];
+
+  return { documentTitle: t('app:letters.page-title'), letters, letterTypes, sortOrder, MSCA_BASE_URL };
 }
 
 export function meta({ data }: Route.MetaArgs) {
   return [{ title: data?.documentTitle }];
 }
 
-export default function Index() {
+export default function LettersIndex({ loaderData, params }: Route.ComponentProps) {
+  // const { currentLanguage } = useLanguage();
+  const [, setSearchParams] = useSearchParams();
   const { t } = useTranslation(handle.i18nNamespace);
 
+  const { letters, letterTypes, sortOrder, MSCA_BASE_URL } = loaderData;
+
+  function handleOnSortOrderChange(e: ChangeEvent<HTMLSelectElement>) {
+    setSearchParams((prev) => {
+      prev.set('sort', e.target.value);
+      return prev;
+    });
+  }
+
   return (
-    <div className="mb-8">
-      <PageTitle className="after:w-14">{t('app:index.page-title')}</PageTitle>
-      <h2 className="mt-10 mb-2 text-2xl font-bold text-slate-700">{t('app:index.about')}</h2>
-    </div>
+    <>
+      <div className="mb-8">
+        <PageTitle className="after:w-14">{t('app:letters.page-title')}</PageTitle>
+      </div>
+      {letters.length === 0 ? (
+        <p>{t('app:letters.no-letter')}</p>
+      ) : (
+        <>
+          <div className="my-6">
+            <InputSelect
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+              id="sort-order"
+              value={sortOrder}
+              onChange={handleOnSortOrderChange}
+              label={t('app:letters.filter')}
+              name="sortOrder"
+              options={[
+                { value: 'desc', children: t('app:letters.newest') },
+                { value: 'asc', children: t('app:letters.oldest') },
+              ]}
+            />
+          </div>
+
+          <ul className="">
+            {letters.map((letter) => {
+              const letterType = letterTypes.find(({ id }) => id === letter.letterTypeId);
+              const gcAnalyticsCustomClickValue = `ESDC-EDSC:CDCP Letters Click:${letterType?.nameEn ?? letter.letterTypeId}`;
+              const letterName = letter.letterTypeId;
+
+              return (
+                <li key={letter.id} className="my-2 rounded px-12 py-4 outline">
+                  <InlineLink
+                    reloadDocument
+                    // TODO ::: Set actual letter route
+                    file="routes/index.tsx"
+                    params={{ ...params, id: letter.id }}
+                    className="external-link"
+                    newTabIndicator
+                    target="_blank"
+                    data-gc-analytics-customclick={gcAnalyticsCustomClickValue}
+                  >
+                    {letterName}
+                  </InlineLink>
+                  <p className="mt-1 text-sm text-gray-500">{t('app:letters.date', { date: letter.date })}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+
+      <div className="my-6 flex flex-wrap items-center gap-3">
+        <ButtonLink id="back-button" to={MSCA_BASE_URL}>
+          {t('app:letters.button.back')}
+        </ButtonLink>
+      </div>
+    </>
   );
 }
