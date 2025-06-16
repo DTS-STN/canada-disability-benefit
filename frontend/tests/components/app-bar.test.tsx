28b370a6850d1c8d21b239d7ae29f82e84f8c1fd
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-06-16 11:33:01 -0230

    tests(frontend): fix broken app-bar tests

--- frontend/tests/components/app-bar.test.tsx
+++ frontend/tests/components/app-bar.test.tsx
@@ -2,11 +2,16 @@ import { createRoutesStub } from 'react-router';
 
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
+import { mock } from 'vitest-mock-extended';
 
 import { AppBar } from '~/components/app-bar';
 
 describe('AppBar', () => {
   it('should correctly render an AppBar with a MenuItem when the file property is provided', () => {
+    globalThis.__appEnvironment = mock({
+      MSCA_BASE_URL: 'https://msca.example.com/',
+    });
+
     const RoutesStub = createRoutesStub([
       {
         path: '/fr/',
@@ -20,6 +25,10 @@ describe('AppBar', () => {
   });
 
   it('should correctly render an AppBar with a MenuItem when the to property is provided', () => {
+    globalThis.__appEnvironment = mock({
+      MSCA_BASE_URL: 'https://msca.example.com/',
+    });
+
     const RoutesStub = createRoutesStub([
       {
         path: '/fr/',
@@ -33,6 +42,10 @@ describe('AppBar', () => {
   });
 
   it('should render render an AppBar with a name provided', () => {
+    globalThis.__appEnvironment = mock({
+      MSCA_BASE_URL: 'https://msca.example.com/',
+    });
+
     const RoutesStub = createRoutesStub([
       {
         path: '/fr/',
@@ -46,6 +59,10 @@ describe('AppBar', () => {
   });
 
   it('should render render an AppBar with a profile item provided', () => {
+    globalThis.__appEnvironment = mock({
+      MSCA_BASE_URL: 'https://msca.example.com/',
+    });
+
     const RoutesStub = createRoutesStub([
       {
         path: '/fr/',
