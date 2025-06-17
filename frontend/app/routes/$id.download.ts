55192c155efcdcfbf63791d1156442e69858ebc9
abalogh-esdc <adam.balogh@hrsdc-rhdcc.gc.ca>
2025-06-17 17:40:10 -0400

    Add security for letter access, UI cleanup

--- frontend/app/routes/$id.download.ts
+++ frontend/app/routes/$id.download.ts
@@ -3,6 +3,7 @@ import { data } from 'react-router';
 
 import type { Route } from './+types/$id.download';
 
+import type { LetterDto } from '~/.server/domain/dtos/letter.dto';
 import { getLetterService } from '~/.server/domain/services/letter.service';
 import { requireAuth } from '~/.server/utils/auth-utils';
 import { AppError } from '~/errors/app-error';
@@ -20,10 +21,24 @@ export async function loader({ context, params, request }: Route.LoaderArgs) {
     throw data(null, { status: 400 });
   }
 
+  // Check if the letters are in the session
+  const { session } = context;
+  const letters: readonly LetterDto[] | undefined = session.letterState;
+  // Optional TODO: add a check to see if the letter belongs to the user. (Done with LetterService call)
+  if (!letters) {
+    throw data(null, { status: 404 });
+  }
+
+  const letter = letters.find((letter) => letter.id === params.id);
+
+  if (!letter) {
+    throw data(null, { status: 404 });
+  }
+
   if (!userinfoTokenClaims.sin) {
     throw new AppError('No SIN found in userinfo token', ErrorCodes.AUTH_USERINFO_FETCH_ERROR);
   }
-  const user = userinfoTokenClaims.sin;
+  const user = userinfoTokenClaims.sub;
 
   const pdfBytes = await getLetterService().getPdfByLetterId({ letterId: params.id, userId: user });
 
